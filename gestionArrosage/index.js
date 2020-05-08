761a68332f7f3cefa06d35e92ae29c2818b6729a
Pablo C (Takkär) <pablocdu22@gmail.com>
2020-05-08 20:14:56 +0200

    Fix importation atob

--- gestionArrosage/index.js
+++ gestionArrosage/index.js
@@ -1,3 +1,5 @@
+global.atob = require("atob");
+
 const farmbotApi = require("../services/farmbotApi"),
     config = require("../config"),
     darksky = require("../services/darksky"),
@@ -5,19 +7,20 @@ const farmbotApi = require("../services/farmbotApi"),
     Farmbot = require("farmbot").Farmbot,
     farmbot = new Farmbot({ token: config.farmbotToken });
 
-global.atob = require("atob");
-
 const errorHandler = (error) => {
     console.log("=== ERROR ===");
     console.dir(error);
-  };
+};
 
 const start = () => {
-    farmbot.connect()
-    .then(() => { console.log("CONNECTED TO FARMBOT!"); })
-    .catch(errorHandler);
-}
+    farmbot
+        .connect()
+        .then(() => {
+            console.log("CONNECTED TO FARMBOT!");
+        })
+        .catch(errorHandler);
+};
 
 module.exports = async function main() {
     await start();
-}
+};
